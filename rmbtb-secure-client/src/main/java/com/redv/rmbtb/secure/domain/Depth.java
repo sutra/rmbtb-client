108487fcf7901cc3bb25831f1ceb07e9d07fff57
Joe Zhou <zhoushuqun@gmail.com>
2013-11-27 21:46:37 +0800

    Move DepthData to Data as an internal class of Depth.

--- rmbtb-secure-client/src/main/java/com/redv/rmbtb/secure/domain/Depth.java
+++ rmbtb-secure-client/src/main/java/com/redv/rmbtb/secure/domain/Depth.java
@@ -1,5 +1,6 @@
 package com.redv.rmbtb.secure.domain;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,9 +15,9 @@ public class Depth extends AbstractObject {
 
 	private final Logger log = LoggerFactory.getLogger(Depth.class);
 
-	private final List<DepthData> asks;
+	private final List<Data> asks;
 
-	private final List<DepthData> bids;
+	private final List<Data> bids;
 
 	public Depth(JSONObject jsonObject) {
 		log.debug("Depth: {}", jsonObject);
@@ -28,26 +29,55 @@ public class Depth extends AbstractObject {
 		bids = new ArrayList<>(bidsJsonArray.size());
 
 		for (Object object : asksJsonArray) {
-			asks.add(new DepthData((JSONArray) object));
+			asks.add(new Data((JSONArray) object));
 		}
 
 		for (Object object : bidsJsonArray) {
-			bids.add(new DepthData((JSONArray) object));
+			bids.add(new Data((JSONArray) object));
 		}
 	}
 
 	/**
 	 * @return the asks
 	 */
-	public List<DepthData> getAsks() {
+	public List<Data> getAsks() {
 		return asks;
 	}
 
 	/**
 	 * @return the bids
 	 */
-	public List<DepthData> getBids() {
+	public List<Data> getBids() {
 		return bids;
+	}
+
+	public class Data extends AbstractObject {
+
+		private static final long serialVersionUID = 2013112401L;
+
+		private final BigDecimal rate;
+
+		private final BigDecimal amount;
+
+		public Data(JSONArray jsonArray) {
+			rate = new BigDecimal((String) jsonArray.get(0));
+			amount = new BigDecimal((String) jsonArray.get(1));
+		}
+
+		/**
+		 * @return the rate
+		 */
+		public BigDecimal getRate() {
+			return rate;
+		}
+
+		/**
+		 * @return the amount
+		 */
+		public BigDecimal getAmount() {
+			return amount;
+		}
+
 	}
 
 }
